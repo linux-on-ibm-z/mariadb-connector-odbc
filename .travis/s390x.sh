a802a725d2d14e20e27bd31a3c21a7d14c4d484f
Nayana Thorat <nthorat@us.ibm.com>
2021-06-22 13:44:07 +0530

    Update s390x.sh

--- .travis/s390x.sh
+++ .travis/s390x.sh
@@ -7,9 +7,10 @@ sudo service mysql stop
 sudo rm -rf /etc/mysql /var/lib/mysql /var/log/mysql
 sudo apt purge -y mysql-server mysql-client mysql-common
 sudo apt autoremove -y
-
-DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server   unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev patch
+sudo ls /etc/systemd/system/
+sudo ls /lib/systemd/
+#DEBIAN_FRONTEND=noninteractive sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev patch
 sudo service mysql start
 sudo mysql -u root -e 'CREATE DATABASE IF NOT EXISTS test;'
 sudo mysql -u root -e "USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES;"
