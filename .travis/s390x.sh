16eba54aba74b371ed739ea002f3d35ecc78f6cb
Nayana Thorat <nthorat@us.ibm.com>
2021-06-23 12:18:56 +0530

    Update s390x.sh

--- .travis/s390x.sh
+++ .travis/s390x.sh
@@ -11,7 +11,7 @@ sudo apt autoremove -y
 sudo apt autoclean -y
  
 #sudo sed -i -e '$aexit 0' /usr/sbin/policy-rc.d
-
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y systemd man-db
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server-10.3
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev patch 
 sudo service mysql start
