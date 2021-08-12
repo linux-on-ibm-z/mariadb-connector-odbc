219debc581a3030924444d92d14c248a78d87d94
Namrata Bhave <Namrata.Bhave@ibm.com>
2021-08-12 14:29:29 +0530

    sudo apt-get purge mysql* mariadb*

--- .travis/s390x.sh
+++ .travis/s390x.sh
@@ -6,7 +6,7 @@ set -e
 DEBIAN_FRONTEND=noninteractive sudo apt-get update
 sudo service mysql stop
 sudo rm -rf /etc/mysql /var/lib/mysql /var/log/mysql
-sudo apt purge -y mysql-server mysql-client mysql-common 
+sudo apt purge -y mysql* mariadb*
 sudo apt autoremove -y
 echo exit 101 | sudo tee /usr/sbin/policy-rc.d
 sudo chmod +x /usr/sbin/policy-rc.d
