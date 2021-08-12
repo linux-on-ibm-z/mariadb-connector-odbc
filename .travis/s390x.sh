4bc3311c6a145469c90473b33474ba62c7a1d164
Namrata Bhave <Namrata.Bhave@ibm.com>
2021-08-12 13:30:43 +0530

    moving   policy-rc.d  back to script

--- .travis/s390x.sh
+++ .travis/s390x.sh
@@ -8,6 +8,8 @@ sudo service mysql stop
 sudo rm -rf /etc/mysql /var/lib/mysql /var/log/mysql
 sudo apt purge -y mysql-server mysql-client mysql-common 
 sudo apt autoremove -y
+echo exit 101 | sudo tee /usr/sbin/policy-rc.d
+sudo chmod +x /usr/sbin/policy-rc.d
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev 
 
 sudo service mysql start
