0542ee09d8c1abbde5219ec735ea4ff378b969f7
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2023-11-18 00:42:55 +0100

    Fix of warnings in 32b build(windows)

--- driver/class/Protocol.cpp
+++ driver/class/Protocol.cpp
@@ -718,7 +718,7 @@ namespace mariadb
       if (totalLenEstimation == 0) {
         totalLenEstimation= firstSql.length()*queries.size() + queries.size() - 1;
       }
-      sql.reserve(((std::min<int64_t>(MAX_PACKET_LENGTH, static_cast<int64_t>(totalLenEstimation)) + 7) / 8) * 8);
+      sql.reserve(static_cast<std::size_t>(((std::min(MAX_PACKET_LENGTH, static_cast<int64_t>(totalLenEstimation)) + 7) / 8) * 8));
       currentIndex= assembleBatchAggregateSemiColonQuery(sql, firstSql, queries, currentIndex);
       realQuery(sql);
       sql.clear(); // clear is not supposed to release memory
